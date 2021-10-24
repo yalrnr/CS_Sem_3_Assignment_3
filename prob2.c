15616db2da0f7f2cfd6f1de28df88a370ec15e84
Anuj Yadav <91149204+yalrnr@users.noreply.github.com>
2021-10-24 17:45:27 +0530

    Update prob2.c

--- prob2.c
+++ prob2.c
@@ -4,7 +4,9 @@ void Calculation(int arr[],int n);
 void InsertionSort(int arr[],int n);
 int main(){
     int n; scanf("%d",&n);
-    int arr[n],i,j; for(i=0;i<n;++i) scanf("%d",&arr[i]);
+    int arr[n],i,j; 
+    for(i=0;i<n;++i) 
+        scanf("%d",&arr[i]);
     Calculation(arr,n);
     return 0;
 }
@@ -56,15 +58,15 @@ void InsertionSort(int arr[],int n){
      case 3- Total clock ticks elapsed for Insertion Sort : 116150
 
 For Insertion Sort, we have-
-      #Worst complexity: n^2           
-      #Average complexity: n^2         
-      #Best complexity: n    
+      # Worst time complexity   :    O(n^2)           
+      # Average time complexity :    O(n^2)         
+      # Best time complexity    :    O(n)    
 Insertion Sort works best when the data is already sorted in ascending order, 
-in this case it does not enter while loop as the condions fail and 
-performs the algorithm in O(n).
-Whereas in case of data sorted in descending order it takes O(n^2) time, which is 
+in this case it does not enter while loop as one of the conditions fails and 
+hence performs the algorithm in O(n).
+Whereas in case of data sorted in descending order, it takes O(n^2) time, which is 
 its worst performance, in this case the loop has to run each time and data is 
-inserted at extreme end, thus taking most time.
-While in case of random data or average case, more often than not it is likely to enter the while
+inserted at extreme end, thus taking the most time.
+While in the case of random data inputs or average cases, more often than not, it is likely to enter the while
 loop thus again performing the algorithm in  O(n^2).
 */
